f128ca7072377c420b4ec087969775bf2c9f20e0
Feichao <fcqian7@gmail.com>
2017-05-18 17:14:44 -0700

    add fields for exercise

--- myapp/models.py
+++ myapp/models.py
@@ -10,6 +10,13 @@ from django.contrib.auth.models import User
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer')
     phone_number = models.IntegerField()
+    is_blacklisted = models.BooleanField(default=False)
+    num_orders = models.IntegerField(default=0)
+
+    def blacklist(self):
+        if self.is_blacklisted:
+            raise Exception("Already in blacklist!")
+        self.is_blacklisted = True
 
     def __unicode__(self):
         return self.user.username
@@ -18,6 +25,13 @@ class Customer(models.Model):
 class FoodItem(models.Model):
     name = models.TextField()
     price = models.IntegerField()
+    num_added = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
+
+    def deactivate(self):
+        if not self.is_active:
+            raise Exception("Already deactivated!")
+        self.is_active = False
 
     def __unicode__(self):
         return self.name
@@ -28,6 +42,8 @@ class Order(models.Model):
     address = models.TextField(default="")
     checkout = models.BooleanField(default=False)
     items = models.ManyToManyField(FoodItem, blank=True, related_name='order', through='OrderItem')
+    deliver_fee = models.IntegerField(default=0)
+    is_delivered = models.BooleanField(default=False)
 
     def __unicode__(self):
         return "Order number: " + repr(self.id)
@@ -36,6 +52,19 @@ class Order(models.Model):
         if self.checkout:
             raise Exception("It's already checked out!")
         self.checkout = True
+
+    def modify_fee(self, fee):
+        self.deliver_fee = fee
+
+    def deliver(self):
+        if self.is_delivered:
+            raise Exception("Already delivered!")
+        self.is_delivered = True
+
+    def revert_delivery(self):
+        if not self.is_delivered:
+            raise Exception("It's not delivered!")
+        self.is_delivered = False
 
 
 class OrderItem(models.Model):
